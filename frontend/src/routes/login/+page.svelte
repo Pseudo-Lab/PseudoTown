f3289b65e680468ed64ab47b9839f50d2d79e5ee
4roring <4roring@naver.com>
2023-11-09 13:54:59 +0900

    로그인시 특성 선택 추가

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -3,9 +3,12 @@
 	import { onMount } from 'svelte';
 	import { Page, Button, List, ListInput, BlockTitle } from 'konsta/svelte';
 	import { apiUrl } from '../globalVars.svelte';
+	import { bingoInfo } from '../bingoInfo.svelte';
 
 	let name = '';
 	let phoneNumber = '';
+	let my_bingo = [0, 0, 0];
+	const excludeBingoSelect = [12, 21, 23];
 
 	onMount(async () => {
 		let user_id = sessionStorage.getItem('user_id');
@@ -22,9 +25,15 @@
 	};
 	const onClickStart = (e) => {
 		console.log(name, phoneNumber);
+		let check_bingo = new Set(my_bingo)
+		if (check_bingo.size != 3)
+		{
+			alert("특성을 중복으로 선택할 수 없습니다.");
+			return
+		}
 		let bodyData = {
 			phone: phoneNumber,
-			bingo: [1, 2, 3]
+			bingo: my_bingo
 		};
 
 		fetch(`${apiUrl}/auth/user/${name}`, {
@@ -56,6 +65,29 @@
 		/>
 		<!-- 빙고판용 성향 필요 -->
 	</List>
+	<List strongIos insetIos>
+		<ListInput label="나를 표현할 수 있는 3가지 - 1" dropdown type="select" onChange={(e) => {my_bingo[0] = e.target.value}}>
+			{#each bingoInfo as data, index (data)}
+				{#if !(excludeBingoSelect.includes(index))} 
+					<option value={index}>{data.value}</option>
+				{/if}
+			{/each}
+		</ListInput>
+		<ListInput label="나를 표현할 수 있는 3가지 - 2" dropdown type="select" onChange={(e) => {my_bingo[0] = e.target.value}}>
+			{#each bingoInfo as data, index (data)}
+				{#if !(excludeBingoSelect.includes(index))} 
+					<option value={index}>{data.value}</option>
+				{/if}
+			{/each}
+		</ListInput>
+		<ListInput label="나를 표현할 수 있는 3가지 - 3" dropdown type="select" onChange={(e) => {my_bingo[0] = e.target.value}}>
+			{#each bingoInfo as data, index (data)}
+				{#if !(excludeBingoSelect.includes(index))} 
+					<option value={index}>{data.value}</option>
+				{/if}
+			{/each}
+		</ListInput>
+	</List >
 	<Button onClick={onClickStart}>네트워킹 시작</Button>
 </Page>
 
